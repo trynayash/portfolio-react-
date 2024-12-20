fcf031debcad8c5408b8ed2f792e52a78938dbbf
Yash Suthar <yashrsuthar90@gmail.com>
2024-12-20 14:55:31 +0530

    portfolio-react

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "YashSuthar";
+const meta = {
+    title: "Yash Suthar",
+    description: "I’m Yash Suthar data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Yash Suthar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I Make Beautiful Web Applications",
+    },
+    description: "Full-Stack Developer, backend development and database management ",
+    your_img_url: "https://media.licdn.com/dms/image/v2/D4D03AQGoVe15FRi0_A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1728997330946?e=1740009600&v=beta&t=PXQqjhOr6fj5C9MIn-QM460hlj2_4XD3eyoYqAwmBkA",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am currently pursuing a Bachelor of Computer Applications (BCA) at Parul University. My studies have honed my skills in backend development, database management, and the full-stack development lifecycle, focusing on Python and related technologies.",
+};
+const worktimeline = [{
+        jobtitle: "Lab Technician - Intern",
+        where: "Parul University",
+        time: "6 Month",
+    },
+    {
+        jobtitle: "Content Creator",
+        where: "Youtube",
+        time: "2021-23",
+    },
+    {
+        jobtitle: "Intern - Data Science (Python)",
+        where: "SmartEd",
+        time: "Dec 2024-Present",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Flask",
+        value: 75,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://db0dce98.rocketcdn.me/en/files/2023/10/fastAPI-1.webp",
+        description: "Fast Api School Vlog Api",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "yashrsuthar90@gmail.com",
+    YOUR_FONE: "+91 9561713120",
+    YOUR_SERVICE_ID: "service_a39bxix",
+    YOUR_TEMPLATE_ID: "template_6n6aaul",
+    YOUR_USER_ID: "czXkKCfgxfyvnESUu",
+};
+
+const socialprofils = {
+    github: "https://github.com/trynayash",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/yxshsuthar",
+    twitter: "https://x.com/yxshsuthar",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
